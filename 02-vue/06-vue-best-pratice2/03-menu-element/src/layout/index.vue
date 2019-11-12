a56a495cd8181d2c98a9fbe6d770cb4be3fef238
super-lin0 <3924578651@qq.com>
2019-11-12 08:21:10 +0800

    菜单树简单版本实现

---
+++ 02-vue/06-vue-best-pratice2/03-menu-element/src/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-wrapper">
+    <side-menu></side-menu>
+    <sidebar></sidebar>
+    <div class="main-container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideMenu from "@/Sidemenu";
+import Sidebar from "@/components/Sidebar";
+
+export default {
+  components: {
+    SideMenu,
+    Sidebar
+  }
+};
+</script>
---
+++ 02-vue/06-vue-best-pratice2/03-menu-element/src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "./permission";
+import "./icons";
+
+import vp from "./directive/permission";
+
+Vue.config.productionTip = false;
+Vue.directive("permission", vp);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
