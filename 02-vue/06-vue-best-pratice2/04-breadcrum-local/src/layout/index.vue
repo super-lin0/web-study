709454e5877fa2352cb5bf820144da8ebae7714d
super-lin0 <3924578651@qq.com>
2019-11-12 19:12:12 +0800

    菜单图标问题

---
+++ 02-vue/06-vue-best-pratice2/04-breadcrum-local/src/layout/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-wrapper">
+    <!-- <side-menu></side-menu> -->
+    <sidebar class="sidebar-container" />
+    <div class="main-container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+// import SideMenu from "@/components/Sidemenu";
+import Sidebar from "@/components/Sidebar";
+
+export default {
+  components: {
+    Sidebar
+    // SideMenu
+  }
+};
+</script>
+
+<style>
+.app-wrapper {
+  display: flex;
+}
+.sidebar-container {
+  width: 20vw;
+  height: 100vh;
+}
+.main-container {
+  width: 80vw;
+  height: 100vh;
+}
+</style>
