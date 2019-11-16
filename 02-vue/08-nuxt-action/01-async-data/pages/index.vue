d4c5b855d95de3bb46e5af79be7ffa7027df9e72
super-lin0 <3924578651@qq.com>
2019-11-16 10:58:14 +0800

    nuxt+koa异步获取数据

--- 02-vue/08-nuxt-action/01-async-data/pages/index.vue
+++ 02-vue/08-nuxt-action/01-async-data/pages/index.vue
@@ -17,23 +17,25 @@
 
 <script>
 export default {
-  data() {
-    return {
-      goods: [
-        { id: 1, text: "Web全栈架构师", price: 8999 },
-        { id: 2, text: "Python全栈架构师", price: 8999 }
-      ]
-    };
-  },
-  head() {
-    return {
-      title: "课程列表",
-      // vue-meta利用hid确定要更新meta
-      meta: [
-        { name: "description", hid: "description", content: "set page meta" }
-      ],
-      link: [{ rel: "favicon", href: "favicon.ico" }]
-    };
+  // data() {
+  //   return {
+  //     goods: [
+  //       { id: 1, text: "Web全栈架构师", price: 8999 },
+  //       { id: 2, text: "Python全栈架构师", price: 8999 }
+  //     ]
+  //   };
+  // },
+  async asyncData({ $axios, error }) {
+    // $axios由nuxtjs/axios模块注入
+    // $get是axios模块封装的类fetch风格的API
+    const { ok, goods } = await $axios.$get("/api/goods");
+
+    if (ok) {
+      // 这里返回的数据将来会和data合并
+      return { goods };
+    }
+    // 错误处理
+    error({ statusCode: 400, message: "数据查询失败" });
   }
 };
 </script>
---
+++ 02-vue/08-nuxt-action/01-async-data/server/api.js
@@ -0,0 +1,46 @@
+const Koa = require("koa");
+const bodyparser = require("koa-bodyparser");
+const router = require("koa-router")({ prefix: "/api" });
+
+const app = new Koa();
+
+// 设置cookie加密秘钥
+app.keys = ["some secret", "another secret"];
+
+const goods = [
+  { id: 1, text: "Web全栈架构师", price: 1000 },
+  { id: 2, text: "Python架构师", price: 1000 }
+];
+
+/**
+ * 配置路由，获取产品列表
+ * url: http://localhost:8080/api/goods
+ */
+router.get("/goods", ctx => {
+  ctx.body = {
+    ok: 1,
+    goods
+  };
+});
+router.get("/detail", ctx => {
+  ctx.body = {
+    ok: 1,
+    data: goods.find(good => good.id == ctx.query.id)
+  };
+});
+router.post("/login", ctx => {
+  const user = ctx.request.body;
+  if (user.username === "jerry" && user.password === "123") {
+    // 将token存入cookie
+    const token = "a mock token";
+    ctx.cookies.set("token", token);
+    ctx.body = { ok: 1, token };
+  } else {
+    ctx.body = { ok: 0 };
+  }
+});
+
+app.use(bodyparser());
+app.use(router.routes());
+
+app.listen(8080, () => console.log("api服务器已经启动"));
