fd4a6fbcee029868cb2317acdf3319e0859c7049
super-lin0 <3924578651@qq.com>
2019-12-12 12:45:32 +0800

    Add React项目测试文档

--- 03-react/09-react-test/02-e2e/cypress/integration/e2e-demo1.spec.js
+++ 03-react/09-react-test/02-e2e/cypress/integration/e2e-demo1.spec.js
@@ -1,10 +1,10 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("抢测试人员饭碗", () => {
   beforeEach(() => {
     cy.visit("localhost:3000");
   });
-  it("Visits the app root url", () => {
+  it("访问首页", () => {
     cy.contains("div", "Hello World");
   });
 });
