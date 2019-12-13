ea06dc533753f0600cef41dfadc081b124d9d359
super-lin0 <3924578651@qq.com>
2019-12-13 12:51:17 +0800

    cli 练手

---
+++ 04-node/01-basic/02-cli/wue-auto-router-cli-exercise/lib/download.js
@@ -0,0 +1,19 @@
+const ora = require("ora");
+
+const { promisify } = require("util");
+
+const download = promisify(require("download-git-repo"));
+
+async function clone(repo, dest) {
+  const process = ora("💍💍 下载 。。。项目");
+  process.start();
+
+  try {
+    await download(repo, dest);
+    process.succeed();
+  } catch {
+    process.fail();
+  }
+}
+
+exports.clone = clone;
