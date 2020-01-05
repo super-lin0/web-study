34b0fa7ae22595ee999213c2b82c94ed32129a1a
super-lin0 <3924578651@qq.com>
2020-01-05 17:58:44 +0800

    Add koa

--- 04-node/09-ts/06-exercise/src/index.ts
+++ 04-node/09-ts/06-exercise/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World ts");
+import * as Koa from "koa";
+import * as bodify from "koa-body";
+import * as serve from "koa-static";
+import * as timing from "koa-xtime";
+
+const app = new Koa();
+
+app.use(timing());
+
+app.use(serve(`${__dirname}/public`));
+
+app.use(
+  bodify({
+    multipart: true,
+    strict: false // 使用非严格模式
+  })
+);
+
+app.use((ctx: Koa.Context) => {
+  ctx.body = "hello ts";
+});
+
+app.listen(3000, () => console.log("服务器启动，端口3000"));
