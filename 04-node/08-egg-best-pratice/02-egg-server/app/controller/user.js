639f0ec33bd8c1af8ddcfad3663330ae9e18b0ac
super-lin0 <3924578651@qq.com>
2020-01-05 11:33:46 +0800

    Add crud

--- 04-node/08-egg-best-pratice/02-egg-server/app/controller/user.js
+++ 04-node/08-egg-best-pratice/02-egg-server/app/controller/user.js
@@ -27,6 +27,73 @@ class UserController extends Controller {
 
     ctx.helper.success({ ctx, res });
   }
+  /**
+   * @summary 删除用户
+   * @description 删除用户
+   * @router delete /api/user/{id}
+   * @request path string *id eg:1 用户ID
+   * @response 200 baseResponse 创建成功
+   */
+  async destroy() {
+    const { ctx, service } = this;
+    const { id } = ctx.params;
+    await service.user.destroy(id);
+    ctx.helper.success({ ctx });
+  }
+
+  /**
+   * @summary 更新用户
+   * @description 修改用户信息
+   * @router put /api/user/{id}
+   * @request path string *id eg: 1 用户ID
+   * @request body createUserRequest * body
+   * @response 200 baseResponse 修改成功
+   */
+  async update() {
+    const { ctx, service } = this;
+    ctx.validate(ctx.rule.createUserRequest);
+
+    console.log("ctx params", ctx.params);
+
+    const { id } = ctx.params;
+
+    const payload = ctx.request.body || {};
+
+    await service.user.update(id, payload);
+
+    ctx.helper.success({ ctx });
+  }
+
+  /**
+   * @summary 根据id查询用户
+   * @description 根据id查询用户
+   * @router get /api/user/{id}
+   * @request path string *id eg:1 用户ID
+   * @response 200 baseResponse 查询成功
+   */
+  async show() {
+    const { ctx, service } = this;
+    const { id } = ctx.params;
+    const res = await service.user.show(id);
+    ctx.helper.success({ ctx, res });
+  }
+
+  /**
+   * @summary 分页查询用户
+   * @description 分页查询用户信息
+   * @router get /api/user/
+   * @request query integer limit eg:10 单页数量
+   * @reuqest query integer offset eg: 1 当前页
+   * @response 200 baseResponse 查询成功
+   */
+  async index() {
+    const { ctx, service } = this;
+
+    const payload = ctx.query;
+
+    const res = await service.user.index(payload);
+    ctx.helper.success({ ctx, res });
+  }
 }
 
 module.exports = UserController;
--- 04-node/08-egg-best-pratice/02-egg-server/app/service/user.js
+++ 04-node/08-egg-best-pratice/02-egg-server/app/service/user.js
@@ -2,6 +2,66 @@ const { Service } = require("egg");
 
 class UserService extends Service {
   /**
+   * 查询用户列表
+   * @param {*} payload
+   */
+  async index({ offset = 1, limit = 10 }) {
+    const { ctx } = this;
+    let skip = (Number(offset) - 1) * Number(limit || 10);
+
+    return await this.ctx.model.User.find({})
+      .populate("role")
+      .skip(skip)
+      .limit(Number(limit))
+      .sort({ createdAt: -1 })
+      .exec();
+  }
+
+  /**
+   * 根据ID查询用户
+   * @param {*} _id
+   */
+  async show(_id) {
+    const user = await this.ctx.service.user.find(_id);
+
+    if (!user) {
+      this.ctx.throw(404, "user not found");
+    }
+
+    return this.ctx.model.User.findById(_id);
+  }
+
+  /**
+   * 根据id删除用户
+   * @param {*} _id
+   */
+  async destroy(_id) {
+    const user = await this.ctx.service.user.find(_id);
+
+    if (!user) {
+      this.ctx.throw(404, "user not found");
+    }
+
+    return this.ctx.model.User.findByIdAndRemove(_id);
+  }
+
+  /**
+   * 修改用户信息
+   * @param {*} id
+   * @param {*} payload
+   */
+  async update(id, payload) {
+    const { ctx } = this;
+    const user = await ctx.service.user.find(id);
+
+    if (!user) {
+      this.ctx.throw(404, "user not found");
+    }
+
+    return this.ctx.model.User.findByIdAndUpdate(id, payload);
+  }
+
+  /**
    * 创建用户
    * @param {*} payload
    */
@@ -10,6 +70,14 @@ class UserService extends Service {
     payload.password = await ctx.genHash(payload.password);
 
     return ctx.model.User.create(payload);
+  }
+
+  /**
+   * 查找用户
+   * @param {*} id
+   */
+  async find(id) {
+    return this.ctx.model.User.findById(id);
   }
 }
 module.exports = UserService;
