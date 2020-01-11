2f16d5634f9727cf1ff8a98f06190b8908fbd9aa
super-lin0 <3924578651@qq.com>
2020-01-11 12:23:41 +0800

    Add

--- 04-node/10-docker/14-cluster/cluster.js
+++ 04-node/10-docker/14-cluster/cluster.js
@@ -24,6 +24,7 @@ if (cluster.isMaster) {
     workers[worker.process.pid] = worker;
   }
 } else {
+  // 工作进程
   const app = require("./app");
   app.listen(3000, () => console.log("服务器启动，端口3000"));
 }
